fbe99ee6a4f26ada598a970f841ceada15ff4a37
Bart Robertson <bartr@microsoft.com>
2021-07-08 06:47:33 +0000

    removed unused code

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -2,11 +2,3 @@
 
 # install cargo debug
 cargo install cargo-debug
-
-# copy grafana.db to /grafana
-sudo mkdir -p /grafana
-sudo  cp deploy/grafanadata/grafana.db /grafana
-sudo  chown -R 472:472 /grafana
-
-# install webv
-dotnet tool install -g webvalidate
